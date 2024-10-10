c3bd50e8add1b1927b42728b6e918b348de07c32
vcky4 <okon.victor.u@gmail.com>
2024-10-10 13:29:11 +0100

    fix: change storage from asyncStorage to redux - useStorage

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, ReactNode } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import useStorage from "../src/hooks/useStorage";
 
 // Define the types for the context and the props
 interface User {
@@ -42,69 +42,44 @@ export const AuthContext = createContext<AuthContextProps>(defaultAuthContext);
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [token, setToken] = useState<string | null>(null);
-  const [user, setUser] = useState<User | null>(null);
-  const [isOnboarded, setIsOnboarded] = useState<boolean>(false);
+  const [token, setToken, removeToken] = useStorage<string | null>('token', null);
+  const [user, setUser, removeUser] = useStorage<User | null>('user', null);
+  const [isOnboarded, setIsOnboarded, clearOnboarded] = useStorage<boolean>('onboarded', false);
 
   const login = (token: string, user: User) => {
     setIsLoading(true);
     setToken(token);
     setUser(user);
-    AsyncStorage.setItem("token", token);
-    AsyncStorage.setItem("user", JSON.stringify(user));
     setIsLoading(false);
   };
 
   const saveToken = (token: string) => {
     setIsLoading(true);
     setToken(token);
-    AsyncStorage.setItem("token", token);
     setIsLoading(false);
   };
 
   const saveUser = (user: User) => {
     setIsLoading(true);
     setUser(user);
-    AsyncStorage.setItem("user", JSON.stringify(user));
     setIsLoading(false);
   };
 
   const onboard = () => {
     setIsOnboarded(true);
-    AsyncStorage.setItem("onboarded", "true");
   };
 
   const logout = () => {
     setIsLoading(true);
-    setToken(null);
-    AsyncStorage.removeItem("token");
-    setUser(null);
-    AsyncStorage.removeItem("user");
+    removeToken();
+    removeUser();
     setIsLoading(false);
   };
 
-  const isLoggedIn = async () => {
-    try {
-      setIsLoading(true);
-      const token = await AsyncStorage.getItem("token");
-      const user = await AsyncStorage.getItem("user");
-      const onboarded = await AsyncStorage.getItem("onboarded");
-
-      setIsOnboarded(onboarded === "true");
-      setToken(token);
-      setUser(user ? JSON.parse(user) : null);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(`isLoggedIn error: ${error}`);
-    }
-  };
+ 
 
   useEffect(() => {
     setTimeout(() => setIsLoading(false), 2000);
-  }, []);
-
-  useEffect(() => {
-    isLoggedIn();
   }, []);
 
   return (
