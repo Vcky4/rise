65d4d188f7b4a27d2e1b16afab3723f8a66ff22a
vcky4 <okon.victor.u@gmail.com>
2024-10-12 13:05:26 +0100

    feet: add plans hook

---
+++ src/hooks/usePlan.ts
@@ -0,0 +1,19 @@
+import IPlans from "../network/models/IPlans";
+import useStorage from "./useStorage";
+
+export function usePlan() {
+    const [plans, setPlans, removePlans] = useStorage<IPlans[] | null>('plans', null);
+
+    const createPlan = (data: IPlans) => {
+        setPlans([...(plans || []), data]);
+    }
+
+    function getPlan(id: string) {
+        return plans?.find((plan) => plan.id === id)
+    }
+
+    return {
+        plans,
+        createPlan
+    }
+}
---
+++ src/network/models/IPlans.ts
@@ -0,0 +1,7 @@
+export default interface IPlans {
+    id: string;
+    goal_name: string;
+    target_amount: number;
+    maturity_date: string;
+    randomId: number
+  }
